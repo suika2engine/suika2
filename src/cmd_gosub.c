085676ce171b762a577783ba7d1b053811e180f8
TABATA Keiichi <tabata.k.ichi@gmail.com>
2018-08-01 22:53:37 +0900

    Fixed forgetting to add new files, and libpng update.

---
+++ src/cmd_gosub.c
@@ -0,0 +1,33 @@
+﻿/* -*- coding: utf-8-with-signature; tab-width: 8; indent-tabs-mode: t; -*- */
+
+/*
+ * Suika 2
+ * Copyright (C) 2001-2018, TABATA Keiichi. All rights reserved.
+ */
+
+/*
+ * [Changes]
+ *  - 2018/07/21 作成
+ */
+
+#include "suika.h"
+
+/*
+ * gosubコマンド
+ */
+bool gosub_command(void)
+{
+	const char *label;
+
+	/* リターンポイントを記録する */
+	push_return_point();
+
+	/* パラメータからラベルを取得する */
+	label = get_string_param(GOSUB_PARAM_LABEL);
+
+	/* ラベルの次のコマンドへ移動する */
+	if (!move_to_label(label))
+		return false;
+
+	return true;
+}
---
+++ src/cmd_return.c
@@ -0,0 +1,42 @@
+﻿/* -*- coding: utf-8-with-signature; tab-width: 8; indent-tabs-mode: t; -*- */
+
+/*
+ * Suika 2
+ * Copyright (C) 2001-2018, TABATA Keiichi. All rights reserved.
+ */
+
+/*
+ * [Changes]
+ *  - 2018/07/22 作成
+ */
+
+#include "suika.h"
+
+/*
+ * returnコマンド
+ */
+bool return_command(void)
+{
+	int rp;
+
+	/* リターンポイントを取得する */
+	rp = pop_return_point();
+
+	/* リターンポイントが無効な場合、エラーとする */
+	if (rp == -1) {
+		/* エラーを出力する */
+		log_script_return_error();
+		log_script_exec_footer();
+		return false;
+	}
+
+	/* リターンポイントの次の行に復帰する */
+	if (!move_to_command_index(rp + 1)) {
+		/* エラーを出力する */
+		log_script_return_error();
+		log_script_exec_footer();
+		return false;
+	}
+
+	return true;
+}
