6222f35147b6a9e8010929dbbe6a3c53b97e37ba
TABATA Keiichi <tabata.keiichi@gmail.com>
2021-04-11 16:30:44 +0900

    Fix palette to RGB conversion

--- src/readimage.c
+++ src/readimage.c
@@ -159,6 +159,9 @@ static bool read_header(void)
 		png_read_update_info(png_ptr, info_ptr);
 		break;
 	case PNG_COLOR_TYPE_PALETTE:
+#if defined(WIN) || defined(LINUX) || defined(FREEBSD) || defined(NETBSD)
+		png_set_bgr(png_ptr);
+#endif
 		png_set_palette_to_rgb(png_ptr);
 		png_set_add_alpha(png_ptr, 0xff, PNG_FILLER_AFTER);
 		png_read_update_info(png_ptr, info_ptr);
