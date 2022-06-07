b466cfb0b1c755704649d8845a6e84cd279c383e
TABATA Keiichi <tabata.keiichi@gmail.com>
2022-06-07 09:55:22 +0900

    Fix Visual Studio manifest

--- src/winmain.c
+++ src/winmain.c
@@ -13,9 +13,7 @@
  */
 
 #define _CRT_SECURE_NO_WARNINGS
-#pragma comment(linker,"\"/manifestdependency:type='win32' \
-name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
-processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 #include "suika.h"
 #include "dsound.h"
