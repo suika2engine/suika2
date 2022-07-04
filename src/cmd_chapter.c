f1e01ef5fcba19788c4a17813c6cb72f62b7a16c
TABATA Keiichi <tabata.keiichi@gmail.com>
2022-07-04 21:26:42 +0900

    Add missing cmd_chapter.c

---
+++ src/cmd_chapter.c
@@ -0,0 +1,31 @@
+﻿/* -*- coding: utf-8-with-signature; tab-width: 8; indent-tabs-mode: t; -*- */
+
+/*
+ * Suika 2
+ * Copyright (C) 2001-2022, TABATA Keiichi. All rights reserved.
+ */
+
+/*
+ * [Changes]
+ *  - 2022/07/04 作成
+ */
+
+#include "suika.h"
+
+/*
+ * 章題設定コマンド
+ */
+bool chapter_command(void)
+{
+	const char *param;
+
+	param = get_string_param(CHAPTER_PARAM_NAME);
+
+	/* 章題を設定する */
+	if (!set_chapter_name(param)) {
+		log_script_exec_footer();
+		return false;
+	}
+
+	return move_to_next_command();
+}
