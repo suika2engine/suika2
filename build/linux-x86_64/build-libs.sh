877aadabcb825b96756db9444bf3300c07e795fa
TABATA Keiichi <tabata.keiichi@gmail.com>
2022-07-25 00:07:21 +0900

    Fix Linux build files

--- build/linux-x86_64/build-libs.sh
+++ build/linux-x86_64/build-libs.sh
@@ -9,43 +9,57 @@ cd tmp
 
 tar xzf ../../libsrc/zlib-1.2.11.tar.gz
 cd zlib-1.2.11
-./configure --prefix=$PREFIX --static
-make
+./configure --prefix=$PREFIX --static CFLAGS='-O3 -ffunction-sections -fdata-sections'
+make -j4
 make install
 cd ..
 
 tar xzf ../../libsrc/libpng-1.6.35.tar.gz
 cd libpng-1.6.35
-./configure --prefix=$PREFIX --disable-shared CPPFLAGS=-I$PREFIX/include LDFLAGS=-L$PREFIX/lib
-make
+./configure --prefix=$PREFIX --disable-shared CPPFLAGS=-I$PREFIX/include CFLAGS='-O3 -ffunction-sections -fdata-sections' LDFLAGS=-L$PREFIX/lib
+make -j4
 make install
 cd ..
 
 tar xzf ../../libsrc/jpegsrc.v9e.tar.gz
 cd jpeg-9e
-./configure --prefix=$PREFIX --disable-shared CPPFLAGS=-I$PREFIX/include LDFLAGS=-L$PREFIX/lib
-make
+./configure --prefix=$PREFIX --disable-shared CPPFLAGS=-I$PREFIX/include CFLAGS='-O3 -ffunction-sections -fdata-sections' LDFLAGS=-L$PREFIX/lib
+make -j4
 make install
 cd ..
 
 tar xzf ../../libsrc/libogg-1.3.3.tar.gz
 cd libogg-1.3.3
-./configure --prefix=$PREFIX --disable-shared
-make
+./configure --prefix=$PREFIX --disable-shared CFLAGS='-O3 -ffunction-sections -fdata-sections'
+make -j4
 make install
 cd ..
 
 tar xzf ../../libsrc/libvorbis-1.3.6.tar.gz
 cd libvorbis-1.3.6
-./configure --prefix=$PREFIX --disable-shared --with-ogg-includes=$PREFIX/include --with-ogg-libraries=$PREFIX/lib
-make
+./configure --prefix=$PREFIX --disable-shared --with-ogg-includes=$PREFIX/include --with-ogg-libraries=$PREFIX/lib CFLAGS='-O3 -ffunction-sections -fdata-sections'
+make -j4
 make install
 cd ..
 
 tar xzf ../../libsrc/freetype-2.9.1.tar.gz
 cd freetype-2.9.1
-./configure --prefix=$PREFIX --disable-shared --with-png=no --with-zlib=no --with-harfbuzz=no --with-bzip2=no
-make
+sed -e 's/FONT_MODULES += type1//' \
+    -e 's/FONT_MODULES += cff//' \
+    -e 's/FONT_MODULES += cid//' \
+    -e 's/FONT_MODULES += pfr//' \
+    -e 's/FONT_MODULES += type42//' \
+    -e 's/FONT_MODULES += winfonts//' \
+    -e 's/FONT_MODULES += pcf//' \
+    -e 's/FONT_MODULES += bdf//' \
+    -e 's/FONT_MODULES += pshinter//' \
+    -e 's/FONT_MODULES += raster//' \
+    -e 's/FONT_MODULES += psaux//' \
+    -e 's/FONT_MODULES += psnames//' \
+    < modules.cfg > modules.cfg.new
+mv modules.cfg.new modules.cfg
+./configure --prefix=$PREFIX --disable-shared --with-png=no --with-zlib=no --with-harfbuzz=no --with-bzip2=no CFLAGS='-O3 -ffunction-sections -fdata-sections'
+make -j4
 make install
 cd ..
 
