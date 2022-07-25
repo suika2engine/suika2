42c0014618753229ce0b7f00bdf74d2d5c04afe9
TABATA Keiichi <tabata.keiichi@gmail.com>
2022-07-25 14:03:25 +0900

    Add build options to mingw-64

--- build/mingw-64/build-libs.sh
+++ build/mingw-64/build-libs.sh
@@ -9,7 +9,7 @@ cd tmp
 
 tar xzf ../../libsrc/zlib-1.2.11.tar.gz
 cd zlib-1.2.11
-make -f win32/Makefile.gcc PREFIX=x86_64-w64-mingw32-
+make -j4 -f win32/Makefile.gcc PREFIX=x86_64-w64-mingw32- CFLAGS='-O3 -ffunction-sections -fdata-sections'
 mkdir -p ../../libroot/include ../../libroot/lib
 cp zlib.h zconf.h ../../libroot/include/
 cp libz.a ../../libroot/lib/
@@ -17,29 +17,50 @@ cd ..
 
 tar xzf ../../libsrc/libpng-1.6.35.tar.gz
 cd libpng-1.6.35
-./configure --prefix=$PREFIX --disable-shared --host=x86_64-w64-mingw32 CPPFLAGS=-I$PREFIX/include LDFLAGS=-L$PREFIX/lib
-make
+./configure --prefix=$PREFIX --disable-shared --host=x86_64-w64-mingw32 CPPFLAGS=-I$PREFIX/include CFLAGS='-O3 -ffunction-sections -fdata-sections' LDFLAGS=-L$PREFIX/lib
+make -j4
+make install
+cd ..
+
+tar xzf ../../libsrc/jpegsrc.v9e.tar.gz
+cd jpeg-9e
+./configure --prefix=$PREFIX --disable-shared --host=x86_64-w64-mingw32 CPPFLAGS=-I$PREFIX/include CFLAGS='-O3 -ffunction-sections -fdata-sections' LDFLAGS=-L$PREFIX/lib
+make -j4
 make install
 cd ..
 
 tar xzf ../../libsrc/libogg-1.3.3.tar.gz
 cd libogg-1.3.3
-./configure --prefix=$PREFIX --host=x86_64-w64-mingw32 --disable-shared
-make
+./configure --prefix=$PREFIX --host=x86_64-w64-mingw32 --disable-shared CFLAGS='-O3 -ffunction-sections -fdata-sections'
+make -j4
 make install
 cd ..
 
 tar xzf ../../libsrc/libvorbis-1.3.6.tar.gz
 cd libvorbis-1.3.6
-./configure --prefix=$PREFIX --host=x86_64-w64-mingw32 --disable-shared --with-ogg-includes=$PREFIX/include --with-ogg-libraries=$PREFIX/lib
-make
+./configure --prefix=$PREFIX --host=x86_64-w64-mingw32 --disable-shared --with-ogg-includes=$PREFIX/include --with-ogg-libraries=$PREFIX/lib CFLAGS='-O3 -ffunction-sections -fdata-sections'
+make -j4
 make install
 cd ..
 
 tar xzf ../../libsrc/freetype-2.9.1.tar.gz
 cd freetype-2.9.1
-./configure --host=x86_64-w64-mingw32 --disable-shared --with-png=no --with-zlib=no --with-harfbuzz=no --with-bzip2=no --prefix=$PREFIX
-make
+sed -e 's/FONT_MODULES += type1//' \
+    -e 's/FONT_MODULES += cff//' \
+    -e 's/FONT_MODULES += cid//' \
+    -e 's/FONT_MODULES += pfr//' \
+    -e 's/FONT_MODULES += type42//' \
+    -e 's/FONT_MODULES += winfonts//' \
+    -e 's/FONT_MODULES += pcf//' \
+    -e 's/FONT_MODULES += bdf//' \
+    -e 's/FONT_MODULES += pshinter//' \
+    -e 's/FONT_MODULES += raster//' \
+    -e 's/FONT_MODULES += psaux//' \
+    -e 's/FONT_MODULES += psnames//' \
+    < modules.cfg > modules.cfg.new
+mv modules.cfg.new modules.cfg
+./configure --host=x86_64-w64-mingw32 --disable-shared --with-png=no --with-zlib=no --with-harfbuzz=no --with-bzip2=no --prefix=$PREFIX CFLAGS='-O3 -ffunction-sections -fdata-sections'
+make -j4
 make install
 cd ..
 
