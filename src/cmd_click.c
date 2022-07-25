244a3128b22b7ea54c0581588780ef0bcaa88684
Keiichi TABATA <tabata.keiichi@gmail.com>
2022-07-25 21:51:42 +0900

    Add timeout for click wait on auto mode

--- src/cmd_click.c
+++ src/cmd_click.c
@@ -12,26 +12,54 @@
 
 #include "suika.h"
 
+/* オートモードのとき待ち時間 */
+#define AUTO_MODE_WAIT	(2000)
+
+static stop_watch_t sw;
+
 /*
  * clickコマンド
  */
 bool click_command(void)
 {
-	/* 入力がない場合はclickコマンドを継続する */
-	if (!is_control_pressed && !is_return_pressed && !is_down_pressed &&
-	    !is_left_button_pressed) {
+	/* 初期化処理を行う */
+	if (!is_in_command_repetition()) {
 		/* メッセージボックスを非表示にする */
 		show_msgbox(false);
 		show_namebox(false);
 
-		/* ステージの描画を維持する */
-		draw_stage_keep();
-		return true;
+		/* スキップモードを終了する */
+		if (is_skip_mode())
+			stop_skip_mode();
+
+		/* 時間の計測を開始する */
+		reset_stop_watch(&sw);
+
+		/* 繰り返し動作を開始する */
+		start_command_repetition();
 	}
+
+	/*
+	 * 入力があった場合か、オートモード中に一定時間経過したら、
+	 * 繰り返し動作を終了する
+	 */
+	if ((!is_auto_mode() &&
+	     (is_control_pressed || is_return_pressed || is_down_pressed ||
+	      is_left_button_pressed))
+	    ||
+	    (is_auto_mode() &&
+	      (float)get_stop_watch_lap(&sw) >= AUTO_MODE_WAIT))
+		stop_command_repetition();
 
 	/* ステージの描画を維持する */
 	draw_stage_keep();
 
-	/* 次のコマンドへ移動する */
-	return move_to_next_command();
+	/* 後処理を行う */
+	if (!is_in_command_repetition()) {
+		/* 次のコマンドへ移動する */
+		return move_to_next_command();
+	}
+
+	/* コマンドの実行を継続する */
+	return true;
 }
