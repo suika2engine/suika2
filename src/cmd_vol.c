e200eb0388a3abd34089c9344e7e95693ba352f0
Keiichi TABATA <tabata.keiichi@gmail.com>
2023-01-08 15:51:18 +0900

    Fixed the track names of the volumes

--- src/cmd_vol.c
+++ src/cmd_vol.c
@@ -32,24 +32,30 @@ bool vol_command(void)
 	}
 
 	if (strcmp(stream, "bgm") == 0 ||
+	    strcmp(stream, "b") == 0 ||
 	    strcmp(stream, "音楽") == 0) {
 		/* BGMストリーム */
 		set_mixer_volume(BGM_STREAM, vol, span);
 	} else if (strcmp(stream, "voice") == 0 ||
+		   strcmp(stream, "v") == 0 ||
 		   strcmp(stream, "声") == 0) {
 		/* VOICEストリーム */
 		set_mixer_volume(VOICE_STREAM, vol, span);
 	} else if (strcmp(stream, "se") == 0 ||
+		   strcmp(stream, "s") == 0 ||
 		   strcmp(stream, "効果音") == 0) {
 		/* SEストリーム */
 		set_mixer_volume(SE_STREAM, vol, span);
-	} else if (strcmp(stream, "BGM") == 0) {
+	} else if (strcmp(stream, "BGM") == 0 ||
+		   strcmp(stream, "B") == 0) {
 		/* BGMグローバルボリューム */
 		set_mixer_global_volume(BGM_STREAM, vol);
-	} else if (strcmp(stream, "VOICE") == 0) {
+	} else if (strcmp(stream, "VOICE") == 0 ||
+		   strcmp(stream, "V") == 0) {
 		/* VOICEグローバルボリューム */
 		set_mixer_global_volume(VOICE_STREAM, vol);
-	} else if (strcmp(stream, "SE") == 0) {
+	} else if (strcmp(stream, "SE") == 0 ||
+		   strcmp(stream, "S") == 0) {
 		/* SEグローバルボリューム */
 		set_mixer_global_volume(SE_STREAM, vol);
 	} else {
