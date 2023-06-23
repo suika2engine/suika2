248af2aeb86acd89a57392e1b0938e40bc51b2f5
Cvar1984 <Cvar1984@pm.me>
2023-06-23 11:56:31 +0700

    add line break

--- build/android/app/src/main/java/jp/luxion/suika/MainActivity.java
+++ build/android/app/src/main/java/jp/luxion/suika/MainActivity.java
@@ -111,7 +111,8 @@ public class MainActivity extends Activity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             getWindow().getDecorView().getWindowInsetsController().setSystemBarsBehavior(
                     WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
-        } if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN
                     |View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     |View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
