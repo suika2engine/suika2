882577e574b09dc9f353b34e569875c0495a73e7
Keiichi Tabata <tabata.keiichi@gmail.com>
2023-10-09 03:06:45 +0900

    build: fix ppa build script

--- build/ppa/create.sh
+++ build/ppa/create.sh
@@ -9,18 +9,20 @@ VERSION=`echo $VERSION | cut -d '/' -f 2`
 
 # Get the minor version.
 MINOR=1
-if [ ! -z "$1" ]; then
+if [ $# != 0 ]; then
 	MINOR="$1";
 fi
 
 # Update the changelog
 if [ -z "`grep $VERSION meta/debian/changelog`" ]; then
-    echo '' >> meta/debian/changelog;
-    echo "suika2 ($VERSION-1) UNRELEASED; urgency=medium" >> meta/debian/changelog;
-    echo '' >> meta/debian/changelog;
-    echo '  * Sync upstream' >> meta/debian/changelog;
-    echo '' >> meta/debian/changelog;
-    echo " -- Keiichi Tabata <tabata@luxion.jp>  `date`" >> meta/debian/changelog;
+    echo "suika2 ($VERSION-1) UNRELEASED; urgency=medium" > meta/debian/changelog.new;
+    echo '' >> meta/debian/changelog.new;
+    echo '  * Sync upstream' >> meta/debian/changelog.new;
+    echo '' >> meta/debian/changelog.new;
+    echo " -- Keiichi Tabata <tabata@luxion.jp>  `date`" >> meta/debian/changelog.new;
+    echo '' >> meta/debian/changelog.new;
+	cat meta/debian/changelog >> meta/debian/changelog.new;
+	mv meta/debian/changelog.new meta/debian/changelog;
 fi
 
 # Make a directory and enter it.
@@ -46,7 +48,7 @@ debuild -S -sa
 cd ..
 
 # Sign.
-debsign -k E9C18AA6087AA39F5114E668EEB70B9FAF1F01C5 suika2_$VERSION-$MINOR_source.changes
+debsign -k E9C18AA6087AA39F5114E668EEB70B9FAF1F01C5 "suika2_${VERSION}-${MINOR}_source.changes"
 
 # Upload.
 dput ppa:ktabata/ppa suika2_$VERSION-$MINOR_source.changes 
