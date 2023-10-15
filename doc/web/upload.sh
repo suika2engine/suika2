a8f0b7ea8f91fd255ea8303eda2d959c04949aa0
Keiichi Tabata <tabata.keiichi@gmail.com>
2023-10-15 16:44:06 +0900

    web: add suika2.com source

---
+++ doc/web/upload.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+function upload()
+{
+    if [ ! -z "$GTAG" ]; then
+        sed -e 's|.*INSERT-GTAG-HERE.*|\<script async src="https://www.googletagmanager.com/gtag/js\?id=G-PLYR5Y3JSJ"></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag("js", new Date());gtag("config", "G-PLYR5Y3JSJ");\</script\>|g' $1 > tmp
+        ftp-upload.sh tmp $1;
+    else
+        ftp-upload.sh $1 $1;
+    fi
+}
+
+upload index.html
+upload dl/index.html
+upload doc/index.html
+upload doc/tutorial.html
+upload doc/reference.html
+upload doc/font.html
+upload doc/title.html
+upload doc/wms.html
+upload doc/scriptgen.html
+upload doc/faq.html
+upload works/index.html
+upload wiki/skin/pukiwiki.skin.php
+
+upload en/index.html
+upload en/dl/index.html
+upload en/doc/index.html
+upload en/doc/tutorial.html
+upload en/doc/reference.html
+upload en/doc/font.html
+upload en/doc/title.html
+upload en/doc/wms.html
+upload en/doc/scriptgen.html
+upload en/doc/faq.html
+upload en/works/index.html
+upload en/wiki/skin/pukiwiki.skin.php
