2cb6ec7a8bc1c30fe40e250861e7dcec784e47e6
Keiichi Tabata <tabata.keiichi@gmail.com>
2023-10-24 22:42:34 +0900

    build: update macOS/iOS build-libs scripts

--- build/ios/build-libs-sim.sh
+++ build/ios/build-libs-sim.sh
@@ -7,6 +7,7 @@ mkdir -p tmp libroot
 
 cd tmp
 
+echo 'building zlib...'
 tar xzf ../../libsrc/zlib-1.2.11.tar.gz
 cd zlib-1.2.11
 CFLAGS="-O3 -arch arm64 -isysroot `xcrun -sdk iphonesimulator --show-sdk-path`" ./configure --prefix=$PREFIX --static --archs="-arch arm64"
@@ -14,6 +15,7 @@ make -j4
 make install
 cd ..
 
+echo 'building libpng...'
 tar xzf ../../libsrc/libpng-1.6.35.tar.gz
 cd libpng-1.6.35
 ./configure --prefix=$PREFIX --host=arm-apple-darwin --disable-shared CPPFLAGS=-I$PREFIX/include CFLAGS="-O3 -arch arm64 -isysroot `xcrun -sdk iphonesimulator --show-sdk-path`" LDFLAGS="-L$PREFIX/lib -arch arm64"
@@ -21,6 +23,23 @@ make
 make install
 cd ..
 
+echo 'building bzip2...'
+tar xzf ../../libsrc/bzip2-1.0.6.tar.gz
+cd bzip2-1.0.6
+make libbz2.a CFLAGS="-O3 -arch arm64 -arch armv7 -isysroot `xcrun -sdk iphonesimulator --show-sdk-path` -fembed-bitcode -mios-version-min=8.0"
+cp bzlib.h ../../libroot/include/
+cp libbz2.a ../../libroot/lib/
+cd ..
+
+echo 'building libwebp...'
+tar xzf ../../libsrc/libwebp-1.3.2.tar.gz
+cd libwebp-1.3.2
+./configure --prefix=$PREFIX --host=arm-apple-darwin --disable-shared CPPFLAGS=-I$PREFIX/include CFLAGS="-O3 -arch arm64 -arch armv7 -isysroot `xcrun -sdk iphonesimulator --show-sdk-path` -fembed-bitcode -mios-version-min=8.0" LDFLAGS="-L$PREFIX/lib -arch arm64 -arch armv7"
+make
+make install
+cd ..
+
+echo 'building jpeg9...'
 tar xzf ../../libsrc/jpegsrc.v9e.tar.gz
 cd jpeg-9e
 ./configure --prefix=$PREFIX --host=arm-apple-darwin --disable-shared CPPFLAGS=-I$PREFIX/include CFLAGS="-O3 -arch arm64 -isysroot `xcrun -sdk iphonesimulator --show-sdk-path`" LDFLAGS="-L$PREFIX/lib -arch arm64"
@@ -28,6 +47,7 @@ make -j4
 make install
 cd ..
 
+echo 'building libogg...'
 tar xzf ../../libsrc/libogg-1.3.3.tar.gz
 cd libogg-1.3.3
 ./configure --prefix=$PREFIX --host=arm-apple-darwin --disable-shared CFLAGS="-O3 -arch arm64 -isysroot `xcrun -sdk iphonesimulator --show-sdk-path`" LDFLAGS="-arch arm64"
@@ -35,6 +55,7 @@ make -j4
 make install
 cd ..
 
+echo 'building libvorbis...'
 tar xzf ../../libsrc/libvorbis-1.3.6.tar.gz
 cd libvorbis-1.3.6
 sed 's/-force_cpusubtype_ALL//' configure > configure.new
@@ -45,6 +66,7 @@ make -j4
 make install
 cd ..
 
+echo 'building freetype2...'
 tar xzf ../../libsrc/freetype-2.9.1.tar.gz
 cd freetype-2.9.1
 sed -e 's/FONT_MODULES += type1//' \
@@ -66,3 +88,5 @@ cd ..
 
 cd ..
 rm -rf tmp
+
+echo 'finished.'
