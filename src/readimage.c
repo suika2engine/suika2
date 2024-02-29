00b3cacd89d2b60c5c3744e1d62cb70f709b6445
Keiichi Tabata <tabata.keiichi@gmail.com>
2024-03-01 05:05:56 +0900

    RGB zero clear

--- src/readimage.c
+++ src/readimage.c
@@ -26,18 +26,38 @@ static bool is_webp_ext(const char *str);
  */
 struct image *create_image_from_file(const char *dir, const char *file)
 {
+	struct image *img;
+	int y, x;
+
 	/* JPEGファイルの場合 */
-	if (is_jpg_ext(file))
-		return create_image_from_file_jpeg(dir, file);
+	if (is_jpg_ext(file)) {
+		img = create_image_from_file_jpeg(dir, file);
+		if (img == NULL)
+			return false;
+		return img;
+	}
 
 #if !defined(NO_WEBP)
 	/* WebPファイルの場合 */
-	if (is_webp_ext(file))
-		return create_image_from_file_webp(dir, file);
+	if (is_webp_ext(file)) {
+		img = create_image_from_file_webp(dir, file);
+		if (img == NULL)
+			return false;
+	}
 #endif
 
 	/* その他の場合はPNGファイルとみなす */
-	return create_image_from_file_png(dir, file);
+	img = create_image_from_file_png(dir, file);
+	if (img == NULL)
+		return false;
+
+	/* 完全に透明なピクセルのRGB値を0にする */
+	for (y = 0; y < img->height; y++)
+		for (x = 0; x < img->width; x++)
+			if (get_pixel_a(img->pixels[y * img->width + x]) == 0)
+				img->pixels[y * img->width + x] = make_pixel(0, 0, 0, 0);
+
+	return img;
 }
 
 /* 拡張子がJPGであるかチェックする */
